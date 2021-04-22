12e6a0a41761880e43a70e60259e93ae2cfffb41
ShirazMuhd <shirazmuhdofficial@gmail.com>
2021-04-22 21:17:10 +0530

    issue fixed

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ var express=require('express')
 var app=express()
 var http = require('http').createServer(app);
 var io=require('socket.io')(http)
-var port = process.env.PORT || 9789
+var port = process.env.PORT || 7000
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/index.html')
 })
 
 io.on('connection',(socket)=>{
-    console.log("new user connected");
 
     socket.on('disconnect',()=>{
-        console.log("Disconnected");
     })
     socket.on("message",(msg)=>{
-        console.log(msg);
         io.emit('in-msg',msg)
     })
 })
@@ -23,6 +20,6 @@ io.on('connection',(socket)=>{
 
 
 http.listen(port,()=>{
-    console.log("server started");
+    console.log("server connencted")
 })
 
