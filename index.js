8ee635d2483948e2f4f81369cd18717e5849ab53
ShirazMuhd <shirazmuhdofficial@gmail.com>
2021-04-23 14:00:40 +0530

    everything fine

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var http = require('http').createServer(app);
 var io=require('socket.io')(http)
 var port = process.env.PORT || 7000
 
+
+app.use(express.static(__dirname+'/public'))
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/public/index.html')
+    res.sendFile('index.html')
 })
 
 io.on('connection',(socket)=>{
